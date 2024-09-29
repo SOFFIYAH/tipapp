17a49af129cd08401515c9bbfe19e74e56367810
Cupcake4fia <56863364+SOFFIYAH@users.noreply.github.com>
2024-09-29 11:33:49 +0100

    3rd commit

--- src/Bill.jsx
+++ src/Bill.jsx
@@ -1,4 +1,4 @@
-import Dollar from "/dolla.svg"
+import dollar from "/dolla.svg"
 import './App.css'
 // eslint-disable-next-line react/prop-types
 const Billz= ({setBill,bill})=>{
@@ -7,7 +7,7 @@ const Billz= ({setBill,bill})=>{
     <div className="do1">
         <label htmlFor="" className="bill">Bill</label>
         <div className="inpbox1">
-            <img src={Dollar} alt="dollar bills" className="imgbill" />
+            <img src={dollar} alt="dollar bills" className="imgbill" />
             <input
                 type="number"
                 name=""
